c31d14b73ec9964a13e4a2a9dca16eb6f0692d55
Lei McKenna <blm5582@rit.edu>
2019-12-12 13:17:00 -0500

    Added new Scene and ship

---
+++ DOTS-Asteroids/Assets/Scripts/ShipController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+
+    Vector2 posVec;
+    Vector2 velocityVec;
+    Rigidbody rigidbodyRef;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbodyRef = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveShip();
+    }
+
+    private void MoveShip()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidbodyRef.AddForce(new Vector3(0.0f, 1.0f, 0.0f));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbodyRef.AddForce(new Vector3(-1.0f, 0.0f, 0.0f));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigidbodyRef.AddForce(new Vector3(0.0f, -1.0f, 0.0f));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigidbodyRef.AddForce(new Vector3(1.0f, 0.0f, 0.0f));
+
+        }
+    }
+
+    private void ShootBullet()
+    {
+        //impliment functions once nick finishes 
+    }
+
+}
